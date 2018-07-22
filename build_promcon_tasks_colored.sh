b8bff5775e61dfecc4421abd3027323a35213b0e
Richard Hartmann <richih@richih.org>
2018-07-22 10:58:43 +0200

    Add error checking
    Signed-off-by: Richard Hartmann <richih@richih.org>

--- build_promcon_tasks_colored.sh
+++ build_promcon_tasks_colored.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+[ -r github.token ] || echo 'file github.token does not exist.' && exit 1
 [ -r github.token ] && export GITHUB_TOKEN=$(cat github.token)
-[ -z GITHUB_TOKEN ] && echo 'set $GITHUB_TOKEN or put it into github.token' && exit 1
-[ -z GOPATH ]       && echo 'set $GOPATH' && exit 2
+[ -z GITHUB_TOKEN ] && echo 'set $GITHUB_TOKEN or put it into github.token' && exit 2
+[ -z GOPATH ]       && echo 'set $GOPATH' && exit 3
 
 $GOPATH/bin/tasks-state-graph
 dot -Tsvg promcon_tasks_colored.dot > promcon_tasks_colored.svg
